fb9846dd2f072bbb43aa6f700c1751da45fc4ed7
josecossi89 <josecossib@gmail.com>
2020-09-27 13:33:35 -0300

    Menu Desplegable

---
+++ myscript.js
@@ -0,0 +1,15 @@
+console.log("Hola a todos este es un mensaje escrito en la consola");
+
+//Variables
+var $nombre = "Jose";
+var $edad = 32;
+var $casado = true;
+var $sueldo = 223030.32;
+
+console.log($nombre);
+console.log($edad);
+console.log($casado);
+console.log($sueldo);
+
+$nombre = 50;
+console.log($nombre);
